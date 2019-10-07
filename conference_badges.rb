0e35a33bdd5c700bbee17a276e0cdbfb3a837217
wolfene2001 <wolfene2001@yahoo.com>
2019-10-07 01:10:32 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badges = []
+  array.each do |attendee|
+    badges << badge_maker(attendee)
+  end
+  return badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  array.each do
+
+end
